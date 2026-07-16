7dea6dbf131ec10b433b32a89ab1aae4e67b1196
padak64 <padak64@naver.com>
2026-07-16 22:38:09 +0900

    level1: 카드 뭉치

---
+++ programmers/level1/카드_뭉치.py
@@ -0,0 +1,15 @@
+"""
+Programmers Level 1
+
+문제: 카드 뭉치
+"""
+
+def solution(cards1, cards2, goal):
+    for g in goal:
+        if cards1 and cards1[0] == g:
+            cards1.pop(0)
+        elif cards2 and cards2[0] == g:
+            cards2.pop(0)
+        else:
+            return "No"
+    return "Yes"
