3d117bfba96d84e44edd9e6d499b77f439386d0b
padak64 <padak64@naver.com>
2026-08-21 10:14:12 +0900

    level2: A Correct Parenthesis

---
+++ programmers/level2/A_Correct_Parenthesis.py
@@ -0,0 +1,16 @@
+"""
+Programmers Level 1
+
+문제: A Correct Parenthesis
+"""
+
+def solution(s):
+    count = 0
+    for ss in s:
+        if ss == "(":
+            count += 1
+        else:
+            count -= 1
+        if count < 0:
+            return False
+    return count == 0
